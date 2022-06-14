fb363adb9d7fe293752769ab6ddb81ac9e287447
Aryse Tansy <medictansy@gmail.com>
2022-06-14 17:57:25 -0400

    Added resume link to about me page

--- components/AboutComps/Card/Card.jsx
+++ components/AboutComps/Card/Card.jsx
@@ -9,8 +9,17 @@ const Card = () => {
       <p className={classes.pDes}>
         Practical, peculiar, solution focused, determined and objective.
       </p>
+
       <p className={classes.pDes}>No non-sense.</p>
+
       <hr className={classes.hr} />
+      <a
+        className={classes.resume}
+        href="https://www.icloud.com/pages/0308deaxC0HL0RI3eKd3W4FkQ#Aryse_Tansy-_Resume"
+        target="_blank"
+      >
+        Resume
+      </a>
     </section>
   );
 };
