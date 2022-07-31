63e228a49889a075733c559856a1603755371859
Aryse Tansy <medictansy@gmail.com>
2022-07-31 14:48:40 -0400

    Edit: Projects

--- components/TechComps/TechBox/TechBox.jsx
+++ components/TechComps/TechBox/TechBox.jsx
@@ -3,18 +3,20 @@ import classes from "./TechBox.module.css";
 const TechBox = () => {
   return (
     <section className={classes.section}>
+      <TechCard techName="Jira" />
+      <TechCard techName="Agile/SCRUM" />
+      <TechCard techName="Git" />
+      <TechCard techName="Github" />
       <TechCard techName="HTML" />
       <TechCard techName="CSS" />
       <TechCard techName="JS" />
       <TechCard techName="NodeJS" />
       <TechCard techName="Express" />
       <TechCard techName="MongoDB" />
-      <TechCard techName="Mongoose" />
-      <TechCard techName="Mongo Atlas" />
       <TechCard techName="Postman" />
+      <TechCard techName="Jest" />
       <TechCard techName="React" />
       <TechCard techName="React-Redux" />
-      <TechCard techName="Redux-Toolkit" />
       <TechCard techName="NextJS" />
     </section>
   );
--- data/projData.js
+++ data/projData.js
@@ -32,8 +32,18 @@ const projData = [
     repo: "https://github.com/Medic1111/my-portal-v2",
     tech: "React, NodeJS, Express, MongoDB, bcrypt, socket.io, Heroku ",
     description:
-      "Full stack app that allows a registered mentor to create a class for which a key is generated. A student with access to that key may enroll and gain reading access to assignments posted by the teacher, as well as engage in real time chat in that particular class. Front end React, chat is possible via Socket.IO, backend with NodeJs, Express, bcrypt for security, and finally, mongoDB for database",
+      "Full stack app that allows a registered mentor to create a class for which a key is generated. A student with access to that key may enroll and gain reading access to assignments posted by the teacher, as well as engage in real time chat in that particular class. ",
     img1: Zone,
+  },
+  {
+    path: "MOOVEES",
+    url: "https://moovees-app.herokuapp.com/",
+    name: "MOOVEES",
+    repo: "https://github.com/Medic1111/MOOVEES-APP",
+    tech: "React, NodeJS, Express, MongoDB, Heroku, imdb API, Jest, Postman, bcrypt",
+    description:
+      "TEST: username and password: testuser; Full stack movie tracker application that allows a registered user to search for movies and tv shows, and organize them into personal lists.",
+    img1: Moovees,
   },
 
   {
@@ -43,18 +53,8 @@ const projData = [
     repo: "https://github.com/Medic1111/food-QR",
     tech: "React, NodeJS, Express, MongoDB, Heroku ",
     description:
-      "Full stack app that allows a food establishment to create an online menu, and generates a QR Code that can be used to enable touchless menu visualization. Created with React, state managed via Context, backend with NodeJs and Express, and mongoDB for database.",
+      "Full stack app that allows a food establishment to create an online menu, and generates a QR Code that can be printed or downloaded enabling touchless menu visualization.",
     img1: FoodQr,
-  },
-  {
-    path: "MOOVEES",
-    url: "https://moovees-app.herokuapp.com/",
-    name: "MOOVEES",
-    repo: "https://github.com/Medic1111/MOOVEES-APP",
-    tech: "React, NodeJS, Express, MongoDB, Heroku, imdb API ",
-    description:
-      "TEST: username and password: testuser; Full stack movie tracker application, that allows a registered user to search for movies and tv shows, and organize them into personal lists.",
-    img1: Moovees,
   },
 
   {
@@ -67,38 +67,38 @@ const projData = [
       "To Test: {cardNumber: 4242 4242 4242 4242, ExDate: 0424, CVC: 242, ZIP: 42424}; Minimalist shop experience for Monstera plant lovers. Built with React, state managed with Redux, payments with Stripe API",
     img1: Monstera,
   },
-  {
-    path: "My Portal",
-    url: "https://testing-portal-api.herokuapp.com/",
-    name: "My Portal",
-    repo: "https://github.com/Medic1111/my-portal",
-    tech: "React, Redux, NodeJS, Express, MongoDB, BCRYPT, Heroku ",
-    description:
-      "To Test: {StudentId: 123456, Password: 123456};  Student portal where you can add your classes and track assignments and grades. Created with Create-React-App with State management via React-Redux and Redux-Toolkit. NodeJS backend with express server and mongoDB for database. Password Hashed and salted with Bcrypt.",
-    img1: MyPortal,
-  },
-  {
-    path: "hands-only",
-    repo: "https://github.com/Medic1111/hands-only",
-    url: "https://hands-only.herokuapp.com/",
-    name: "Hands-Only",
-    tech: "ReactJS, NodeJS, Express, MailChimp API, MongoDB",
-    description:
-      "To Test: {answers: c-b-c-a-c-a-b-c-a}; App created to boots efforts of helping peolple to provide hands-only CPR in case of cardiac-arrest. Quiz style with 9 explanations and questions which at the end allows for newsletter subscription. Front end done with React, state management via useContext hook, NodeJS back end with express server and MongoDB for database. MailChimp API integration for newsletter campaign.",
-    img1: HandsOnly,
-  },
+  // {
+  //   path: "My Portal",
+  //   url: "https://testing-portal-api.herokuapp.com/",
+  //   name: "My Portal",
+  //   repo: "https://github.com/Medic1111/my-portal",
+  //   tech: "React, Redux, NodeJS, Express, MongoDB, BCRYPT, Heroku ",
+  //   description:
+  //     "To Test: {StudentId: 123456, Password: 123456};  Student portal where you can add your classes and track assignments and grades. Created with Create-React-App with State management via React-Redux and Redux-Toolkit. NodeJS backend with express server and mongoDB for database. Password Hashed and salted with Bcrypt.",
+  //   img1: MyPortal,
+  // },
+  // {
+  //   path: "hands-only",
+  //   repo: "https://github.com/Medic1111/hands-only",
+  //   url: "https://hands-only.herokuapp.com/",
+  //   name: "Hands-Only",
+  //   tech: "ReactJS, NodeJS, Express, MailChimp API, MongoDB",
+  //   description:
+  //     "To Test: {answers: c-b-c-a-c-a-b-c-a}; App created to boots efforts of helping peolple to provide hands-only CPR in case of cardiac-arrest. Quiz style with 9 explanations and questions which at the end allows for newsletter subscription. Front end done with React, state management via useContext hook, NodeJS back end with express server and MongoDB for database. MailChimp API integration for newsletter campaign.",
+  //   img1: HandsOnly,
+  // },
 
-  {
-    path: "mario",
-    repo: "https://github.com/Medic1111/Mario-Smack",
-    url: "https://mario-whack.netlify.app/",
-    name: "Mario Whack",
-    tech: "HTML, CSS, JS",
-    description:
-      "Don't add this to your portfolio, they said. When learning web development, one of the common projects to tackle is the boring 'Whack a mole' game. I was never a fan of it and skipped it. Then, I was able to move a button. Literally, move a button at key press. That gave me a sense of 'Anything is possible' and along came my desktop version of whack a mole. Deciding to add it to my portfolio is based on my belief of 'you cannot forget where you started'. It's also the reason why I left the code UNTOUCHED, exactly as it was on my 3rd week of learning JS. Note: Nintendo owns everything related to Mario Bros. I'm just a fan of the plumber.",
+  // {
+  //   path: "mario",
+  //   repo: "https://github.com/Medic1111/Mario-Smack",
+  //   url: "https://mario-whack.netlify.app/",
+  //   name: "Mario Whack",
+  //   tech: "HTML, CSS, JS",
+  //   description:
+  //     "Don't add this to your portfolio, they said. When learning web development, one of the common projects to tackle is the boring 'Whack a mole' game. I was never a fan of it and skipped it. Then, I was able to move a button. Literally, move a button at key press. That gave me a sense of 'Anything is possible' and along came my desktop version of whack a mole. Deciding to add it to my portfolio is based on my belief of 'you cannot forget where you started'. It's also the reason why I left the code UNTOUCHED, exactly as it was on my 3rd week of learning JS. Note: Nintendo owns everything related to Mario Bros. I'm just a fan of the plumber.",
 
-    img1: Mario,
-  },
+  //   img1: Mario,
+  // },
 ];
 
 export default projData;
